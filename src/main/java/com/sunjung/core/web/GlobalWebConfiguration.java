6df1a7392cafd9eb3be887f81315a10eca308883
DESKTOP-N8P93O1\为 <2521018380@qq.com>
2017-04-21 17:36:19 +0800

    edit

--- src/main/java/com/sunjung/core/web/GlobalWebConfiguration.java
+++ src/main/java/com/sunjung/core/web/GlobalWebConfiguration.java
@@ -13,13 +13,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class GlobalWebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/ace/");
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
         super.addResourceHandlers(registry);
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/index").setViewName("index");
         super.addViewControllers(registry);
     }
 }
---
+++ src/main/resources/static/js/base.js
@@ -0,0 +1,37 @@
+
+
+/**
+ * 请求完成如果操作失败,提示信息
+ * @param responseText
+ */
+function afterCompleteMsg(data){
+    var res = $.parseJSON(data.responseText);
+    if(res.operateCode == "S")
+        jQuery("#grid-table").trigger("reloadGrid");
+
+    if(res.operateCode == "F")
+        layer.open({
+            content: res.message,
+            scrollbar: false
+        });
+}
+
+
+function commonCompleteMsg(data,gridTable){
+    if(data.operateCode == "S")
+        jQuery("#"+gridTable).trigger("reloadGrid");
+
+    if(data.operateCode == "F")
+        layer.open({
+            content: data.message,
+            scrollbar: false
+        });
+}
+
+function commonCompleteMsg(data){
+    if(data.operateCode == "F")
+        layer.open({
+            content: data.message,
+            scrollbar: false
+        });
+}
